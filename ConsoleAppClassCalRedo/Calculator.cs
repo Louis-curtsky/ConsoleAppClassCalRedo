b579318b6683ff8f41434a945e1e81d4185770f5
Louis Lim <curtsky@outlook.com>
2022-02-22 10:43:53 +0100

    Add project files.

---
+++ ConsoleAppClassCalRedo/Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppClassCalRedo
+{
+    public class Calculator
+    {
+        public int NumberFrom;
+        public int NumberTo;
+
+        private double douNum;
+        private double douNumBy;
+
+        public double DouNumBy
+        { 
+            get { return douNumBy; } 
+            set { douNumBy = Math.Round(DouNumBy, 2); }
+        }
+            
+        public double DouNum
+        {
+            get { return douNum; }
+            set { douNum = Math.Round(douNum, 2); }
+        }
+
+        public Calculator()
+        { 
+        }
+
+        public Calculator(int numberFrom, int numberTo)
+        {
+            NumberFrom = numberFrom;
+            NumberTo = numberTo;
+        }
+
+        public Calculator(double dounumber, double dounumberBy)
+        {
+            DouNum = dounumber;
+            DouNumBy = dounumberBy;
+        }
+        public int Addition(int numberFrom, int numberTo)
+        {
+            this.NumberFrom = numberFrom;
+            this.NumberTo = numberTo;
+            return (numberFrom+numberTo);
+        }
+
+        public int Addition(params int[] numbers)
+        {
+            var sums = 0;
+            foreach (var number in numbers)
+            {
+                sums += number;
+            }
+            return sums;
+        }
+
+        
+        public int Substraction(int subtrent)
+        {
+            int Answer = (NumberFrom - subtrent);
+            return Answer;
+        }
+
+        public double multiplication(double douNum, double doubNumBy)
+        {
+            this.DouNum = douNum;
+            this.DouNumBy = douNumBy;
+            double Answer = douNum * douNumBy;
+            return Answer;
+        }
+
+
+        public double Division(double divideNum, double divideBy)
+        {
+            if (divideBy == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(divideBy));
+            }
+            this.DouNum = Math.Round(divideNum, 2);
+            this.DouNumBy = Math.Round(divideBy,2);
+            double Answer = (divideNum / divideBy);
+            return Math.Round(Answer,2);
+        }
+
+    }// End Cal Con 1
+
+
+}
---
+++ ConsoleAppClassCalRedo/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleAppClassCalRedo;
+
+namespace ConsoleAppClassCalRedo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            do
+            {
+                PrintMenu();
+
+                int assignmentSel = int.Parse(Console.ReadLine() ?? "0");
+
+                if (assignmentSel < 6 && assignmentSel > 0)
+                {
+                    MathOperation(assignmentSel);
+                }
+                else if (assignmentSel == 9)
+                {
+                    keepGoing = false;
+                    Console.WriteLine("Good bye!!!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid selection !!!");
+                }
+                Console.ResetColor();
+                Console.WriteLine("Press any key to continue !");
+                // key press hidden with (True)
+                Console.ReadKey();
+                Console.Clear();
+            } while (keepGoing);
+        }// End of Main
+
+        static void MathOperation(int selection)
+        {
+            var sum = new Calculator();
+            int tot = 0;
+            bool listInputEnd = false;
+                     double InputD1 = 0;
+                     double InputD2 = 0;
+                  var sumDou = new Calculator(InputD1,InputD2);
+
+            List<int> numberList = new List<int>();   
+            switch (selection)
+            {
+                case 1:
+                    int firstnum = UserInputNumber("Enter 1st number");
+                    int secondnum = UserInputNumber("Enter 2nd number");
+                    Console.WriteLine(firstnum + " + " + secondnum + " is = "+sum.Addition(firstnum, secondnum));
+                    break;
+                case 2:
+                    while (listInputEnd == false)
+                    {
+                        int addToListm = UserInputNumber("Add number to list (0 to end): ");
+                        if (addToListm == 0)
+                        {
+                            listInputEnd = true;
+                        }
+                        else
+                        {
+                            numberList.Add(addToListm);
+                        }
+                    }
+                            for (int i = 0; i < numberList.Count; i++)
+                            {
+                                tot += sum.Addition(numberList[i]);
+                            }
+                    Console.WriteLine(tot);
+  //                  Console.WriteLine("Sum List is = "+sum);
+                    break;
+                case 3:
+                    Console.WriteLine(" is = ");
+                    break;
+                case 4:
+                     Console.WriteLine(" is = ");
+                    break;
+                case 5:
+                    InputD1 = 45.67777;
+                    InputD2 = 3.67778;
+
+                    Console.WriteLine($"{InputD1} / {InputD2} equal to {sumDou.Division(InputD1,InputD2)}");
+                    break;
+            }
+        }
+        static int UserInputNumber(string desc)
+        {
+            string inputed = UserInputStr(desc);
+            int number = Convert.ToInt32(inputed);
+            return number;
+        }
+        
+        static double UserInputDouble(string desc)
+        {
+            string inputed = UserInputStr(desc);
+            double number = Convert.ToDouble(inputed);
+            return number;
+        }
+
+        static string UserInputStr(string userinputstr)
+        {
+            Console.Write("Please enter " + userinputstr + ": ");
+            return Console.ReadLine();
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("----- Menu -----");
+            Console.WriteLine("1: ADD Two numbers");
+            Console.WriteLine("2: ADD more than TWO numbers");
+            Console.WriteLine("3: MULTIPLICATION Operation");
+            Console.WriteLine("5: DIVISION Operation");
+            Console.WriteLine("9: Exit program");
+            Console.WriteLine("\nEnter you choice: ");
+        }
+
+    }
+}
